64f2c4468e355d343bb2a9a38964b354cb2dd3a0
Arnav Gupta <arnav@codingblocks.com>
2017-09-03 05:18:27 +0530

    add pods support
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'ember-learn',
+    podModulePrefix: 'ember-learn/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
