719d2bc68d1fd28749231af44711b4a2ee08a730
Arnav Gupta <arnav@codingblocks.com>
2017-09-03 05:44:22 +0530

    add products and products/:id routes
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('products', function() {
+    this.route('id', {path: '/:id'});
+  });
 });
 
 export default Router;
